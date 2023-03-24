d5d0962e75141d9e0dcc138c25f70c56b05cc963
Valeria <lja.18n@gmail.com>
2023-03-24 09:37:15 +0200

    test commit

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  // css: {
+  //   preprocessorOptions: {
+  //     scss: {
+  //       additionalData: `
+  //         @import "./src/assets/styles/general/_reset.scss";
+  //         @import "./src/assets/styles/general/_mixins.scss";
+  //       `
+  //     }
+  //   }
+  // },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
